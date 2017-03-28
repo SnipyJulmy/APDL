6d4b50e515ebad44ae0fffebfc7b4d60a41b1018
SnipyJulmy <sylvain.julmy@gmail.com>
2017-03-28 10:55:43 +0200

    Add utility for the stream

--- src/main/scala/apdl/core/package.scala
+++ src/main/scala/apdl/core/package.scala
@@ -10,6 +10,7 @@ package object math {
 }
 
 package object Utils {
+
   implicit class ApdlByte(byte: Byte) {
     def cHex: String = "0x%02X" format byte
   }
@@ -22,24 +23,29 @@ package object ApdlStreamManager {
   private val main = new File("apdl-main.c")
   main.delete()
   private val ApdlMainStream = new PrintWriter(main)
+  mainPrintln(s"// APDL MAIN")
 
   // Header stream
   private val header = new File("apdl-gen.h")
   header.delete()
   private val ApdlHeaderStream = new PrintWriter(header)
+  headerPrintln(s"// APDL HEADER")
 
   // Stream for the Loop function from arduino
   private val loop = new File("apdl-loop.h")
   loop.delete()
   private val ApdlLoopStream = new PrintWriter(loop)
+  loopPrintln(s"// APDL LOOP")
+  loopPrintln(s"timer.update();")
 
   // Stream for the Setup function from arduino
   private val setup = new File("apdl-setup.h")
-  loop.delete()
-  private val ApdlSetupStream = new PrintWriter(loop)
+  setup.delete()
+  private val ApdlSetupStream = new PrintWriter(setup)
+  setupPrintln(s"// APDL SETUP")
 
   private def apdlPrintln(stream: PrintWriter)(s: String): Unit = {
-    stream.append(s).flush()
+    stream.append(s + "\n").flush()
   }
 
   def mainPrintln(s: String): Unit = {
@@ -58,8 +64,8 @@ package object ApdlStreamManager {
     apdlPrintln(ApdlSetupStream)(s)
   }
 
-  def headerStream = ApdlHeaderStream
-  def mainStream = ApdlMainStream
-  def loopStream = ApdlLoopStream
-  def setupStream = ApdlSetupStream
+  def apdlHeaderStream = ApdlHeaderStream
+  def apdlMainStream = ApdlMainStream
+  def apdlLoopStream = ApdlLoopStream
+  def apdlSetupStream = ApdlSetupStream
 }
