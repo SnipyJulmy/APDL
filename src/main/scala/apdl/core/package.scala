b2ff312cbd1f7bff20979a222e6020294c399c1a
SnipyJulmy <sylvain.julmy@gmail.com>
2017-03-29 10:50:23 +0200

    Add additionnal stream for the function header file Add a directory for the generated file

--- src/main/scala/apdl/core/package.scala
+++ src/main/scala/apdl/core/package.scala
@@ -19,30 +19,42 @@ package object Utils {
 }
 
 package object ApdlStreamManager {
+  private val directory = new File(s"apdl-gen")
+  if (directory.exists()) directory.delete()
+  directory.mkdirs()
+  private val directoryPath = directory.getAbsolutePath
+
   // Main stream
-  private val main = new File("apdl-main.c")
+  private val main = new File(s"$directoryPath/apdl-gen.ino")
   main.delete()
   private val ApdlMainStream = new PrintWriter(main)
   mainPrintln(s"// APDL MAIN")
 
   // Header stream
-  private val header = new File("apdl-gen.h")
+  private val header = new File(s"$directoryPath/apdl-header.h")
   header.delete()
   private val ApdlHeaderStream = new PrintWriter(header)
   headerPrintln(s"// APDL HEADER")
 
   // Stream for the Loop function from arduino
-  private val loop = new File("apdl-loop.h")
+  private val loop = new File(s"$directoryPath/apdl-loop.h")
   loop.delete()
   private val ApdlLoopStream = new PrintWriter(loop)
   loopPrintln(s"// APDL LOOP")
   loopPrintln(s"timer.update();")
 
   // Stream for the Setup function from arduino
-  private val setup = new File("apdl-setup.h")
+  private val setup = new File(s"$directoryPath/apdl-setup.h")
   setup.delete()
   private val ApdlSetupStream = new PrintWriter(setup)
   setupPrintln(s"// APDL SETUP")
+
+  // Stream for the function of the Arduino, include after the header
+  // for the global variable
+  private val function = new File(s"$directoryPath/apdl-fun.h")
+  function.delete()
+  private val ApdlFunctionStream = new PrintWriter(function)
+  functionPrintln(s"// APDL Function")
 
   private def apdlPrintln(stream: PrintWriter)(s: String): Unit = {
     stream.append(s + "\n").flush()
@@ -64,8 +76,13 @@ package object ApdlStreamManager {
     apdlPrintln(ApdlSetupStream)(s)
   }
 
+  def functionPrintln(s : String) : Unit = {
+    apdlPrintln(ApdlFunctionStream)(s)
+  }
+
   def apdlHeaderStream = ApdlHeaderStream
   def apdlMainStream = ApdlMainStream
   def apdlLoopStream = ApdlLoopStream
   def apdlSetupStream = ApdlSetupStream
+  def apdlFunctionStream = ApdlFunctionStream
 }
