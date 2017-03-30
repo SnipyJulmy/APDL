581a8d7c68ca557d8b372763add0db251032d4a7
SnipyJulmy <sylvain.julmy@gmail.com>
2017-03-30 13:42:49 +0200

    Add some utility class

--- src/main/scala/apdl/core/APDL.scala
+++ src/main/scala/apdl/core/APDL.scala
@@ -6,5 +6,16 @@ package apdl.core
   */
 trait APDL extends Dsl {
 
+  case class InfluxSender(dbName: String, sourceName: String) {
+    def sendInt(data: Rep[Int], topic: String, sampling: Int): Exp[Unit] = sendIntToInfluxDb(data, dbName, sourceName, topic, sampling)
+    def sendFloat(data: Rep[Float], topic: String, sampling: Int): Exp[Unit] = sendFloatToInfluxDb(data, dbName, sourceName, topic, sampling)
+  }
+
+  case class InfluxTopic(dbName: String, sourceName: String, topic: String) {
+    def sendInt(data: Rep[Int], sampling: Int): Exp[Unit] = sendIntToInfluxDb(data, dbName, sourceName, topic, sampling)
+    def sendFloat(data: Rep[Float], sampling: Int): Exp[Unit] = sendFloatToInfluxDb(data, dbName, sourceName, topic, sampling)
+  }
 }
+
+
 
