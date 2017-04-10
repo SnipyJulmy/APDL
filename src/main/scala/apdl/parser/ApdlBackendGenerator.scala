11e315295a01d855b0634d4dccbc10892e5ab25a
SnipyJulmy <sylvain.julmy@gmail.com>
2017-04-10 09:23:30 +0200

    Generation of the transformation

--- src/main/scala/apdl/parser/ApdlBackendGenerator.scala
+++ src/main/scala/apdl/parser/ApdlBackendGenerator.scala
@@ -7,6 +7,8 @@ trait ApdlBackendGenerator {
 }
 
 class ArduinoGenerator extends ApdlBackendGenerator {
+
+
   override def generate(entities: List[Entity]): String = {
     val main = new StringWriter
     val loop = new StringWriter
@@ -14,8 +16,9 @@ class ArduinoGenerator extends ApdlBackendGenerator {
     val function = new StringWriter
     val header = new StringWriter
 
-    val servers = entities.filter(o => o.isInstanceOf[Server]).map(o => o.asInstanceOf[Server])
+    val servers = entities.filter(_.isInstanceOf[Server]).map(_.asInstanceOf[Server])
     val sources = entities.filter(_.isInstanceOf[Source]).map(_.asInstanceOf[Source])
+    val transformaters = entities.filter(_.isInstanceOf[Transformater]).map(_.asInstanceOf[Transformater])
 
     // generate servers info
     // the database is global or not for all server ?
@@ -41,11 +44,71 @@ class ArduinoGenerator extends ApdlBackendGenerator {
       }
     }
 
+    // generate transformater
+    transformaters.foreach { tf =>
+      function.write {
+        generate(tf.function)
+      }
+    }
+
     println(s"$main \n $header \n $function \n $loop \n $setup")
 
     // TODO include previous StringWriter
     s"""
        |
      """.stripMargin
+  }
+
+  def generate(expr: Expr): String = expr match {
+    case Add(left, right) => s"(${generate(left)} + ${generate(right)})"
+    case Mul(left, right) => s"(${generate(left)} * ${generate(right)})"
+    case Sub(left, right) => s"(${generate(left)} - ${generate(right)})"
+    case Div(left, right) => s"(${generate(left)} / ${generate(right)})"
+    case Literal(number) => s"${generate(number)}"
+    case Constant(name) => s" $name "
+    case Symbol(name) => s" $name "
+    case Number(value) => s" $value "
+    case m: MathFunction => m match {
+      case Log(operand) => s"(log(${generate(operand)}))"
+      case Log10(operand) => s"(log10(${generate(operand)}))"
+      case Sin(operand) => s"(sin(${generate(operand)}))"
+      case Cos(operand) => s"(cos(${generate(operand)}))"
+      case Tan(operand) => s"(tan(${generate(operand)}))"
+      case Asin(operand) => s"(asin(${generate(operand)}))"
+      case Acos(operand) => s"(acos(${generate(operand)}))"
+      case Atan(operand) => s"(atan(${generate(operand)}))"
+      case Exp(operand) => s"(exp(${generate(operand)}))"
+      case Sqrt(operand) => s"(sqrt(${generate(operand)}))"
+      case Ceil(operand) => s"(ceil(${generate(operand)}))"
+      case Floor(operand) => s"(floor(${generate(operand)}))"
+      case Pow(n, power) => s"(pow(${generate(n)},${generate(power)}))"
+      case Round(operand) => s"(round(${generate(operand)}))"
+      case Abs(operand) => s"(abs(${generate(operand)}))"
+      case Max(left, right) => s"(max(${generate(left)},${generate(right)}))"
+      case Min(left, right) => s"(min(${generate(left)},${generate(right)}))"
+      case Tanh(operand) => s"(tanh(${generate(operand)}))"
+      case Sinh(operand) => s"(sinh(${generate(operand)}))"
+      case Cosh(operand) => s"(cosh(${generate(operand)}))"
+    }
+  }
+
+  def generate(retType: TfTyp): String = retType match {
+    case TfInt() => "int"
+    case TfFloat() => "float"
+    case TfLong() => "long"
+    case TfDouble() => "double"
+    case TfVoid() => "void"
+  }
+
+  def generate(statement: Statement): String = statement match {
+    case TfDef(name, args, retType, statements, ret) =>
+      s"""
+         | ${generate(retType)} $name (${args map (a => s"${generate(a.typ)} ${a.name}") mkString ","}) {
+         |   ${statements map generate mkString ";\n"}
+         |   return ${generate(ret)};
+         | }
+        """.stripMargin
+    case TfNewVal(name, typ, init) =>
+      s"${generate(typ)} $name = ${generate(init)};"
   }
 }
