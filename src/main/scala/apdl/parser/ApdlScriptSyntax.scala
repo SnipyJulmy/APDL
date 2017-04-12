005fd844d28804ddb904e915078e840f3f10c346
SnipyJulmy <sylvain.julmy@gmail.com>
2017-04-12 10:09:19 +0200

    Add some apply method for convinience, remove math function

--- src/main/scala/apdl/parser/ApdlScriptSyntax.scala
+++ src/main/scala/apdl/parser/ApdlScriptSyntax.scala
@@ -1,12 +1,12 @@
 package apdl.parser
 
-sealed trait ApdlTfSyntax
+sealed trait Expr
 
-sealed trait Expr extends ApdlTfSyntax
 case class Add(left: Expr, right: Expr) extends Expr
 case class Mul(left: Expr, right: Expr) extends Expr
 case class Sub(left: Expr, right: Expr) extends Expr
 case class Div(left: Expr, right: Expr) extends Expr
+
 case class Literal(number: Number) extends Expr
 case class Constant(name: String) extends Expr
 case class Symbol(name: String) extends Expr
@@ -15,7 +15,7 @@ case class FunctionCall(funcName: String, args: List[Expr]) extends Expr
 
 sealed trait Assignement extends Statement
 case class VarAssignement(name: String, value: Expr) extends Assignement
-case class ArrayAssignement(name : String, field : Expr, value : Expr) extends Assignement
+case class ArrayAssignement(name: String, field: Expr, value: Expr) extends Assignement
 
 sealed trait BooleanExpr extends Expr
 case class True() extends BooleanExpr
@@ -24,28 +24,12 @@ case class Or(left: BooleanExpr, right: BooleanExpr) extends BooleanExpr
 case class And(left: BooleanExpr, right: BooleanExpr) extends BooleanExpr
 case class Not(booleanExpr: BooleanExpr) extends BooleanExpr
 case class BooleanSymbol(name: String) extends BooleanExpr
-
-sealed trait MathFunction extends Expr
-case class Log(expr: Expr) extends MathFunction
-case class Log10(expr: Expr) extends MathFunction
-case class Sin(expr: Expr) extends MathFunction
-case class Cos(expr: Expr) extends MathFunction
-case class Tan(expr: Expr) extends MathFunction
-case class Asin(expr: Expr) extends MathFunction
-case class Acos(expr: Expr) extends MathFunction
-case class Atan(expr: Expr) extends MathFunction
-case class Exp(expr: Expr) extends MathFunction
-case class Sqrt(expr: Expr) extends MathFunction
-case class Ceil(expr: Expr) extends MathFunction
-case class Floor(expr: Expr) extends MathFunction
-case class Pow(n: Expr, power: Expr) extends MathFunction
-case class Round(expr: Expr) extends MathFunction
-case class Abs(expr: Expr) extends MathFunction
-case class Max(left: Expr, right: Expr) extends MathFunction
-case class Min(left: Expr, right: Expr) extends MathFunction
-case class Tanh(expr: Expr) extends MathFunction
-case class Sinh(expr: Expr) extends MathFunction
-case class Cosh(expr: Expr) extends MathFunction
+case class Greater(left: Expr, right: Expr) extends BooleanExpr
+case class Smaller(left: Expr, right: Expr) extends BooleanExpr
+case class GreaterEquals(left: Expr, right: Expr) extends BooleanExpr
+case class SmallerEquals(left: Expr, right: Expr) extends BooleanExpr
+case class Equals(left: Expr, right: Expr) extends BooleanExpr
+case class NotEquals(left: Expr, right: Expr) extends BooleanExpr
 
 sealed trait TfRetTyp
 sealed trait TfTyp extends TfRetTyp
@@ -75,10 +59,9 @@ case class NewVal(identifier: String, typ: TfTyp, init: Expr) extends Declaratio
 case class NewVar(identifier: String, typ: TfTyp, init: Option[Expr]) extends Declaration
 
 case class NewArray(identifier: String, typ: TfArray, init: ArrayInit) extends Declaration
-case class ArrayInit(values : List[TypedIdentifier])
+case class ArrayInit(values: List[TypedIdentifier])
 
-
-sealed trait Statement extends ApdlTfSyntax
+sealed trait Statement
 case class ExpressionStatement(expression: Expr) extends Statement
 case class While(cond: BooleanExpr, statement: Statement) extends Statement
 case class DoWhile(cond: BooleanExpr, statement: Statement) extends Statement
@@ -88,3 +71,49 @@ case class Return(expr: Expr) extends Statement
 case class Break() extends Statement
 case class Continue() extends Statement
 case class Block(statements: List[Statement]) extends Statement
+
+// Companion object
+
+object Add {
+  def apply(l: AnyVal, r: AnyVal): Add = new Add(Literal(Number(l.toString)), Literal(Number(r.toString)))
+  def apply(l: String, r: AnyVal): Add = new Add(Symbol(l), Literal(Number(r.toString)))
+  def apply(l: AnyVal, r: String): Add = new Add(Literal(Number(l.toString)), Symbol(r))
+  def apply(l: Expr, r: AnyVal): Add = new Add(l, Literal(Number(r.toString)))
+  def apply(l: AnyVal, r: Expr): Add = new Add(Literal(Number(l.toString)), r)
+  def apply(l: Expr, r: String): Add = new Add(l, Symbol(r))
+  def apply(l: String, r: Expr): Add = new Add(Symbol("l"), r)
+  def apply(l: String, r: String): Add = new Add(Symbol("l"), Symbol("r"))
+}
+
+object Mul {
+  def apply(l: AnyVal, r: AnyVal): Mul = new Mul(Literal(Number(l.toString)), Literal(Number(r.toString)))
+  def apply(l: String, r: AnyVal): Mul = new Mul(Symbol(l), Literal(Number(r.toString)))
+  def apply(l: AnyVal, r: String): Mul = new Mul(Literal(Number(l.toString)), Symbol(r))
+  def apply(l: Expr, r: AnyVal): Mul = new Mul(l, Literal(Number(r.toString)))
+  def apply(l: AnyVal, r: Expr): Mul = new Mul(Literal(Number(l.toString)), r)
+  def apply(l: Expr, r: String): Mul = new Mul(l, Symbol(r))
+  def apply(l: String, r: Expr): Mul = new Mul(Symbol("l"), r)
+  def apply(l: String, r: String): Mul = new Mul(Symbol("l"), Symbol("r"))
+}
+
+object Sub {
+  def apply(l: AnyVal, r: AnyVal): Sub = new Sub(Literal(Number(l.toString)), Literal(Number(r.toString)))
+  def apply(l: String, r: AnyVal): Sub = new Sub(Symbol(l), Literal(Number(r.toString)))
+  def apply(l: AnyVal, r: String): Sub = new Sub(Literal(Number(l.toString)), Symbol(r))
+  def apply(l: Expr, r: AnyVal): Sub = new Sub(l, Literal(Number(r.toString)))
+  def apply(l: AnyVal, r: Expr): Sub = new Sub(Literal(Number(l.toString)), r)
+  def apply(l: Expr, r: String): Sub = new Sub(l, Symbol(r))
+  def apply(l: String, r: Expr): Sub = new Sub(Symbol("l"), r)
+  def apply(l: String, r: String): Sub = new Sub(Symbol("l"), Symbol("r"))
+}
+
+object Div {
+  def apply(l: AnyVal, r: AnyVal): Div = new Div(Literal(Number(l.toString)), Literal(Number(r.toString)))
+  def apply(l: String, r: AnyVal): Div = new Div(Symbol(l), Literal(Number(r.toString)))
+  def apply(l: AnyVal, r: String): Div = new Div(Literal(Number(l.toString)), Symbol(r))
+  def apply(l: Expr, r: AnyVal): Div = new Div(l, Literal(Number(r.toString)))
+  def apply(l: AnyVal, r: Expr): Div = new Div(Literal(Number(l.toString)), r)
+  def apply(l: Expr, r: String): Div = new Div(l, Symbol(r))
+  def apply(l: String, r: Expr): Div = new Div(Symbol("l"), r)
+  def apply(l: String, r: String): Div = new Div(Symbol("l"), Symbol("r"))
+}
