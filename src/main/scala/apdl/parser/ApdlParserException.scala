d10fe2e943736a3af27e80ea9aaa7d24a3a647e1
SnipyJulmy <sylvain.julmy@gmail.com>
2017-04-13 13:21:01 +0200

    Add an exception for a DSL error, like non-specification of an obligatory parammeters (ip,mac,...)

--- src/main/scala/apdl/parser/ApdlParserException.scala
+++ src/main/scala/apdl/parser/ApdlParserException.scala
@@ -1,5 +1,11 @@
 package apdl.parser
 
+class ApdlDslException(s: String) extends Throwable {
+  override def toString: String = {
+    s"$s ${super.toString}"
+  }
+}
+
 class ApdlParserException(s: String) extends Throwable {
   override def toString: String = {
     s"$s ${super.toString}"
