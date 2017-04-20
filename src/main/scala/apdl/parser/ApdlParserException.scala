5150a30bd949b4c7e8e7eb20a815a7eafdb54b03
SnipyJulmy <sylvain.julmy@gmail.com>
2017-04-20 10:39:17 +0200

    Add empty constructor for test usage

--- src/main/scala/apdl/parser/ApdlParserException.scala
+++ src/main/scala/apdl/parser/ApdlParserException.scala
@@ -1,18 +1,24 @@
 package apdl.parser
 
 class ApdlDslException(s: String) extends Throwable {
+  def this() = this("")
+
   override def toString: String = {
     s"$s ${super.toString}"
   }
 }
 
 class ApdlParserException(s: String) extends Throwable {
+  def this() = this("")
+
   override def toString: String = {
     s"$s ${super.toString}"
   }
 }
 
-class ApdlArgsException(s : String) extends Throwable {
+class ApdlArgsException(s: String) extends Throwable {
+  def this() = this("")
+
   override def toString: String = {
     s"$s ${super.toString}"
   }
