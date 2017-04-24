8a56ef89a59fa9de55e9b627debf43834a7667f5
SnipyJulmy <sylvain.julmy@gmail.com>
2017-04-24 14:10:39 +0200

    Use Enumeration for ApdlTarget

--- src/main/scala/apdl/parser/ApdlTarget.scala
+++ src/main/scala/apdl/parser/ApdlTarget.scala
@@ -1,11 +1,7 @@
 package apdl.parser
 
-sealed trait ApdlTarget
-object ApdlTarget {
-  def arg2target(arg: String): ApdlTarget = arg match {
-    case "arduino" => Arduino
-    case _ => throw new ApdlArgsException(s"Unsupported target : $arg")
-  }
+object ApdlTarget extends Enumeration {
+  type ApdlTarget = Value
+  val Arduino = Value
+  def default : ApdlTarget = Arduino
 }
-
-object Arduino extends ApdlTarget
