3be7bf3000b138c26100646535d03c1370431ee9
SnipyJulmy <sylvain.julmy@gmail.com>
2017-05-07 16:02:56 +0200

    Arg parsing, small try for new DSL based on TOML

--- src/main/scala/apdl/parser/Main.scala
+++ src/main/scala/apdl/parser/Main.scala
@@ -2,55 +2,21 @@ package apdl.parser
 
 import java.io.File
 
-import apdl.parser.ApdlTarget.ApdlTarget
+import apdl.parser.ApdlGenerationType.ApdlGenerationType
 import com.github.SnipyJulmy.scalacolor.ScalaColor._
 
-import scala.io.Source
 import scala.language.postfixOps
-import scala.util.Try
-import scala.util.parsing.input.CharSequenceReader
 
-case class ApdlConfig(file: File = new File("."),
-                      target: ApdlTarget = ApdlTarget.default,
-                      outputDirectory: File = new File("./default-apdl-output"))
+case class ApdlConfig(mainFile: File = new File("."),
+                      outputDirectory: File = new File("./default-apdl-output"),
+                      generationType: ApdlGenerationType = ApdlGenerationType.default)
 
 object Main extends App {
 
-  val config = parse(args)
-  val parser = new ApdlParser
-
-  import parser._
-
-  // TODO optimize result, verification, semantic analysis
-
-  val codeStr: String = {
-    Source.fromFile(config.file).mkString
-  }
-  val code = parser.parse(parser.program, new PackratReader[Char](new CharSequenceReader(codeStr)))
-  code match {
-    case Success(result, _) =>
-      // TODO specify generator
-      val backend = new ArduinoGenerator
-      Try {
-        backend.generate(config.outputDirectory, result)
-      } match {
-        case util.Failure(exception) => exception match {
-          case e: ApdlBackendException =>
-            println(s"Generation failed : ${e.getMessage}".red)
-            System.exit(1)
-          case _ =>
-            println(exception)
-        }
-        case util.Success(_) =>
-          println("Compilation end successfully".blue)
-      }
-    case error: NoSuccess =>
-      println(s"Parse error : $error".red)
-  }
+  val apdlParser = new MainParser(parse(args))
+  println(apdlParser.parseFile())
 
   def parse(args: Array[String]): ApdlConfig = {
-    implicit val apdlTargetRead: scopt.Read[ApdlTarget.Value] =
-      scopt.Read.reads(ApdlTarget withName)
 
     val argsParser = new scopt.OptionParser[ApdlConfig]("apdl") {
       // Metadata
@@ -58,12 +24,7 @@ object Main extends App {
 
       // Argument
       arg[File]("<file>")
-        .action((f, c) => c.copy(file = f))
-
-      // Optional args
-      opt[ApdlTarget]('t', "target")
-        .action((t, c) => c.copy(target = t))
-        .text("targeting framework")
+        .action((f, c) => c.copy(mainFile = f))
 
       opt[File]('d', "dir")
         .action((o, c) => c.copy(outputDirectory = o))
@@ -73,6 +34,7 @@ object Main extends App {
     argsParser.parse(args, ApdlConfig()) match {
       case Some(value) => value
       case None =>
+        println("Unable to parse args".red)
         System.exit(0)
         throw new Exception("Unreachable code")
     }
