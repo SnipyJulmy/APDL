d29a4425590736690c92f93519d174d105cf5515
SnipyJulmy <sylvain.julmy@gmail.com>
2017-05-08 10:45:42 +0200

    Refactor + use toml4j

--- src/main/scala/apdl/parser/MainParser.scala
+++ src/main/scala/apdl/parser/MainParser.scala
@@ -3,10 +3,9 @@ package apdl.parser
 import java.io.StringWriter
 
 import apdl.parser.parser.{ComponentsParser, TypesParser}
-import fastparse.core.Parsed.{Failure, Success}
-import stoml.Toml._
-import stoml.TomlParserApi._
+import com.moandjiezana.toml.Toml
 
+import scala.collection.JavaConverters._
 import scala.io.Source
 
 case class ApdlProject(name: String,
@@ -20,61 +19,20 @@ class MainParser(apdlConfig: ApdlConfig) {
    */
 
   def parseFile(): ApdlProject = {
-    val mainAst = parseToml(apdlConfig.mainFile) match {
-      case Success(value, _) => value
-
-      case failure: Failure[_, _] => throw new ApdlParserException(s"failed parse ${apdlConfig.mainFile.getPath} : $failure")
-    }
-    val project = ApdlProject(
-      parseProjectName(mainAst),
-      parseDependencies(mainAst)
-    )
+    val toml = new Toml().read(apdlConfig.mainFile)
+    val dependencies = toml.getList[String]("dependencies").asScala.toList
+    val projectName = toml.getString("project_name")
 
     val full = new StringWriter()
     full.append(Source.fromFile(apdlConfig.mainFile).mkString)
-    project.dependencies.foreach { filename =>
+    dependencies.foreach { filename =>
       val path = apdlConfig.mainFile.getParentFile.getPath + "/" + filename
-      full.append(Source.fromFile(path).mkString)
+      full.append("\n" + Source.fromFile(path).mkString)
     }
-    // The full toml ast
-    val ast = parseToml(full.toString) match {
-      case Success(value, _) => value
-      case failure: Failure[_, _] => throw new ApdlParserException(s"failed parse full generated file : $failure")
-    }
-    println(ast)
-    val types = TypesParser(ast)
-    val components = ComponentsParser(ast)
-    project
+
+    val fullToml = new Toml().read(full.toString)
+    val types = TypesParser(fullToml)
+    val components = ComponentsParser(fullToml)
+    ApdlProject(projectName, dependencies)
   }
-
-  private def parseProjectName(ast: TomlContent): String = ast.lookup("project_name") match {
-    case Some(value) => value match {
-      case Pair((_, pairValue)) => pairValue match {
-        case Str(projectName) => projectName
-        case a => throw new ApdlFormatException(s"Expected string, found ${a.getClass}")
-      }
-      case Table(_) => throw new ApdlFormatException("Found table for project_name key, expected string")
-    }
-    case None => throw new ApdlFormatException("No project_name specified")
-  }
-
-  private def parseDependencies(ast: TomlContent): List[String] = {
-    val dependencies: List[String] = ast.lookup("dependencies") match {
-      case Some(value) => value match {
-        case Pair((_, pairValue)) => pairValue match {
-          case Arr(elems) => elems.map {
-            case Str(str) => str
-            case _ => throw new ApdlFormatException("Dependencies parameters type mismatch, expected string")
-          }.toList
-          case _ => throw new ApdlFormatException("Dependencies info ins't in an array")
-        }
-        case Table(_) => throw new ApdlFormatException("Found table, expected a pair")
-      }
-      case None => List()
-    }
-    dependencies
-  }
-
-
-
 }
