b09193f6c8d0303a37e287951c0a3520a8e9cc0d
SnipyJulmy <sylvain.julmy@gmail.com>
2017-05-08 10:45:22 +0200

    Use toml4j for type parsing

--- src/main/scala/apdl/parser/parser/TypesParser.scala
+++ src/main/scala/apdl/parser/parser/TypesParser.scala
@@ -1,62 +1,35 @@
 package apdl.parser.parser
 
-import apdl.parser._
-import stoml.Toml._
-import stoml.TomlParserApi._
+import com.moandjiezana.toml.Toml
+import scala.collection.JavaConverters._
 
 object TypesParser {
-  def apply(ast: TomlContent): Map[String, TypesParser#ApdlType] = (new TypesParser).parseTypes(ast)
-}
-
-class TypesParser {
-  private def parseTypes(ast: TomlContent): Map[String, ApdlType] = {
-
-    val types = ast.childOf("type").toList.map {
-      case Pair(_) => throw new ApdlFormatException(s"Expected type in an array, not in pair")
-      case Table((k, m)) =>
-        k.toString -> ApdlType(
-          format(m),
-          requirements(m),
-          declarators(m)
-        )
-    }
-    types.toMap
-  }
-
-  def format(m: Map[String, Elem]): String = m(typeValueKey) match {
-    case Str(str) => str
-    case x => throw new ApdlFormatException(s"Expected string in the value, got ${x.getClass}")
-  }
-
-  def requirements(m: Map[String, Elem]): List[String] = m(typeRequireKey) match {
-    case Arr(seq) => seq map {
-      case Str(str) => str
-      case x => throw new ApdlFormatException(s"Expected string in the require array value, got ${x.getClass}")
-    } toList
-    case x => throw new ApdlFormatException(s"Expected an array for require, got ${x.getClass}")
-  }
-
-  def declarators(m: Map[String, Elem]): Map[String, String] = m(typeDeclaratorsKey) match {
-    case Arr(seq) => seq.map {
-      case Arr(seq2) => // 2 field always
-        assert(seq2.length == 2)
-        val target = seq2.head match {
-          case Str(str) => str
-          case x3 => throw new ApdlFormatException(s"Expected a string for declarators key, got ${x3.getClass}")
-        }
-        val code = seq2.tail.head match {
-          case Str(str) => str
-          case x3 => throw new ApdlFormatException(s"Expected a string for declarators value, got ${x3.getClass}")
-        }
-        target -> code
-      case x2 => throw new ApdlFormatException(s"Expected an array of array for declarators, got ${x2.getClass}")
-    }.toMap
-    case x => throw new ApdlFormatException(s"Expected an array for declarators, got ${x.getClass}")
-  }
-
   private val typeValueKey = "value"
   private val typeRequireKey = "require"
   private val typeDeclaratorsKey = "declarators"
 
-  case class ApdlType(format: String, requirement: List[String], declarators: Map[String, String])
+  def apply(toml: Toml): Map[String, ApdlType] = (new TypesParser).parseTypes(toml)
 }
+
+class TypesParser {
+
+  import TypesParser._
+
+  private def parseTypes(toml: Toml): Map[String, ApdlType] = {
+    val types = toml.getTable("type")
+    val keys = types.toMap.keySet().asScala.toList
+    keys.map { k =>
+      val value = types.getTable(k).getString(typeValueKey)
+      val require = types.getTable(k).getList[String](typeRequireKey).asScala.toList
+      val decl = types.getTable(k).getList[java.util.ArrayList[String]](typeDeclaratorsKey).asScala.toList
+      val declarators = decl.map { arl =>
+        val l = arl.asScala.toList
+        assert(l.length == 2)
+        l.head -> l.tail.head
+      }.toMap
+      k -> ApdlType(value, require, declarators)
+    }.toMap
+  }
+}
+
+case class ApdlType(format: String, requirement: List[String], declarators: Map[String, String])
