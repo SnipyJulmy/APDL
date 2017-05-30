a0cd1f4000df939a88492a41682072b288d27aee
SnipyJulmy <sylvain.julmy@gmail.com>
2017-05-30 15:37:18 +0200

    Update utils for debug and exitOnFailure

--- src/main/scala/apdl/ApdlUtils.scala
+++ src/main/scala/apdl/ApdlUtils.scala
@@ -1,26 +1,34 @@
 package apdl
 
 object ApdlUtils {
-  def debug(str : String)(implicit debugEnable : Boolean) : Unit = {
-    if(debugEnable) {
+  def debug(str: String)(implicit debugEnable: Boolean): Unit = {
+    if (debugEnable) {
       println(str)
     }
   }
 
-  def warning(str : String)(implicit debugEnable : Boolean) : Unit = {
-    if(debugEnable) {
+  def warning(str: String)(implicit debugEnable: Boolean): Unit = {
+    if (debugEnable) {
       println(str)
     }
   }
 
-  def exitOnFailure() : Unit = {
+  def exitOnFailure(): Unit = {
     println(s"EXIT with code 1")
     System.exit(1)
   }
 
-  def exitOnFailure(msg : String)(implicit debugEnable : Boolean) : Unit = {
-    if(debugEnable)
+  def exitOnFailure(msg: String)(implicit debugEnable: Boolean): Unit = {
+    if (debugEnable)
       println(msg)
+    println(s"EXIT with code 1")
+    System.exit(1)
+  }
+
+  def exitOnFailure(exception: Throwable)(implicit debugEnable: Boolean): Unit = {
+    if(debugEnable){
+      exception.printStackTrace(System.out)
+    }
     println(s"EXIT with code 1")
     System.exit(1)
   }
