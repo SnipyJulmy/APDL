60d8689802832d25a8ddea37c5ecf4b95bce56e2
SnipyJulmy <sylvain.julmy@gmail.com>
2017-06-01 13:56:02 +0200

    Move the cli args parser the a ApdlArgsParser.scala file

--- src/main/scala/apdl/ApdlArgsParser.scala
+++ src/main/scala/apdl/ApdlArgsParser.scala
@@ -3,7 +3,29 @@ package apdl
 import java.io.File
 
 class ApdlArgsParser {
+  def parse(args: Array[String]): ApdlConfig = {
 
+    val argsParser = new scopt.OptionParser[ApdlConfig]("apdl") {
+      // Metadata
+      head("apdl", "1.0")
+
+      // Argument
+      arg[File]("<file>.apdl")
+        .action((f, c) => c.copy(mainFile = f))
+
+      opt[File]('d', "dir")
+        .action((o, c) => c.copy(outputDirectory = o))
+        .text("Output directory")
+    }
+
+    argsParser.parse(args, ApdlConfig()) match {
+      case Some(value) => value
+      case None =>
+        println("Unable to parse args")
+        System.exit(0)
+        throw new Exception("Unreachable code")
+    }
+  }
 }
 
 case class ApdlConfig(mainFile: File = new File("."),
--- src/main/scala/apdl/Main.scala
+++ src/main/scala/apdl/Main.scala
@@ -16,30 +16,4 @@ object Main extends App {
   val generator = new ProjectGenerator(manager.project)
   generator.mkProject()
   debug("End")
-  /*
-
-  def parse(args: Array[String]): ApdlConfig = {
-
-    val argsParser = new scopt.OptionParser[ApdlConfig]("apdl") {
-      // Metadata
-      head("apdl", "1.0")
-
-      // Argument
-      arg[File]("<file>")
-        .action((f, c) => c.copy(mainFile = f))
-
-      opt[File]('d', "dir")
-        .action((o, c) => c.copy(outputDirectory = o))
-        .text("Output directory")
-    }
-
-    argsParser.parse(args, ApdlConfig()) match {
-      case Some(value) => value
-      case None =>
-        println("Unable to parse args".red)
-        System.exit(0)
-        throw new Exception("Unreachable code")
-    }
-  }
-  */
 }
