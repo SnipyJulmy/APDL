275b8b1e31150739bbb2bf8cde7fbb82770c08a0
SnipyJulmy <sylvain.julmy@gmail.com>
2017-07-03 14:50:33 +0200

    Add handler generation for any number of device and any number of serial send

--- src/main/scala/apdl/generation/ApdlHandler.scala
+++ src/main/scala/apdl/generation/ApdlHandler.scala
@@ -4,9 +4,6 @@ import java.io.{File, PrintWriter, StringWriter}
 
 import apdl.parser.ApdlProject
 import apdl.{ApdlCodeGenerationException, ApdlConfig}
-import scala.Function._
-
-import sys.process._
 
 class ApdlHandler(val project: ApdlProject)(implicit config: ApdlConfig) {
 
@@ -15,72 +12,70 @@ class ApdlHandler(val project: ApdlProject)(implicit config: ApdlConfig) {
   generateSource()
 
   def generateSerials(): List[HandlerSerial] = {
-    val serials = project.devices.flatMap(d => d.serials.map(s => (d,s)))
-    serials map tupled { (d,s) =>
-      val id = d.name
-      val topic = s.inputName
-      val port = d.port
-      HandlerSerial(id,topic,port)
-    }
+    val serials = project.devices.flatMap(d => d.serials.map(s => (d, s))).groupBy(_._1)
+    serials.map { entry =>
+      val device = entry._1
+      val serials = entry._2.map(_._2)
+
+      val id = device.name
+      val topics = serials.map(_.inputName)
+      val port = device.port
+
+      HandlerSerial(id, topics, port)
+    } toList
   }
 
   def generateSource(): Unit = {
     val serials: List[HandlerSerial] = generateSerials()
-    println(
-      s"""
-import serial
-
-from influxdb import InfluxDBClient
-
-def process(serial, client, compValue, deviceName):
-  data = serial.readline()
-  asciiData: str = data.decode('ascii')
-  array = asciiData.split(":")
-  topic: str = array[0]
-  value: str = array[1]
-  value = value.replace("\\n", "", 1)
-  value = value.replace("\\r", "", 1)
-  value = value.replace(" ", "")
-  topic = topic.replace(" ", "")
-  if compValue not in topic:
-    return
-  print("Send to influxdb == " + topic + " : " + value)
-  json = [
-      {
-          "measurement": topic + deviceName,
-          "fields": {
-              "value": int(value)
-          }
-      }
-  ]
-  client.write_points(json)
-
-def main():
-  ${serials.map(_.mkSerialDeclaration).mkString("\n  ")}
-
-  user = 'root'
-  password = 'root'
-  dbname = 'apdl-default'
-
-  client = InfluxDBClient('localhost', 8086, user, password, dbname)
-  client.drop_database(dbname)
-  print("Create database")
-  client.create_database(dbname)
-
-  # The format is
-  # topic : value
-
-  while True:
-    try:
-      ${serials.map(_.mkProcessCall).mkString("\n    ")}
-    except IndexError:
-      continue
-    except serial.serialutil.SerialException:
-      continue
-
-if __name__ == "__main__":
-  main()
-""")
+    printlnSW(
+      s"""|import serial
+          |from influxdb import InfluxDBClient
+          |
+          |def process(serial, client, compValue, deviceName):
+          |  data = serial.readline()
+          |  asciiData: str = data.decode('ascii')
+          |  array = asciiData.split(":")
+          |  topic: str = array[0]
+          |  value: str = array[1]
+          |  value = value.replace("\\n", "", 1)
+          |  value = value.replace("\\r", "", 1)
+          |  value = value.replace(" ", "")
+          |  topic = topic.replace(" ", "")
+          |  for comp in compValue :
+          |    if comp in topic :
+          |      print("Send to influxdb == " + topic + " : " + value)
+          |      json = [
+          |        {
+          |          "measurement": topic + deviceName,
+          |          "fields": {
+          |            "value": int(value)
+          |          }
+          |        }
+          |      ]
+          |      client.write_points(json)
+          |
+          |def main():
+          |  ${serials.map(_.mkSerialDeclaration).mkString("\n  ")}
+          |  user = 'root'
+          |  password = 'root'
+          |  dbname = 'apdl-default'
+          |
+          |  client = InfluxDBClient('localhost', 8086, user, password, dbname)
+          |  client.drop_database(dbname)
+          |  print("Create database")
+          |  client.create_database(dbname)
+          |
+          |  while True:
+          |    try:
+          |      ${serials.map(_.mkProcessCall).mkString("\n      ")}
+          |    except IndexError:
+          |      continue
+          |    except serial.serialutil.SerialException:
+          |      continue
+          |
+          |if __name__ == "__main__":
+          |  main()
+          |""".stripMargin('|'))
   }
 
   def mkFile(rootDirectory: File): Unit = {
@@ -95,30 +90,19 @@ if __name__ == "__main__":
       }
     }
     val pw = new PrintWriter(file)
-    pw.append(outputString.toString)
+    pw.print(outputString.toString)
     pw.flush()
     pw.close()
-    formatSource(file)
   }
 
-  private def println(string: String): Unit = {
-    outputString.append(string + "\n")
+  private def printlnSW(string: String): Unit = {
+    outputString.append(string)
     outputString.flush()
-  }
-
-  private def formatSource(file : File): Unit = {
-    val filename = file.getAbsolutePath
-    val command = s"astyle --delete-empty-lines --suffix=none --style=linux --break-blocks=all $filename"
-    println(s"Invoke : $command")
-
-    val result = command !!
-
-    println(result)
   }
 }
 
-case class HandlerSerial(id: String, topicName: String, port: String, baud: Int = 9600, byteSize: Int = 8, timeout: Int = 1) {
+case class HandlerSerial(id: String, topicNames: List[String], port: String, baud: Int = 9600, byteSize: Int = 8, timeout: Int = 1) {
   def mkSerialDeclaration: String = s"""$id = serial.Serial('$port',$baud,bytesize=$byteSize,timeout=$timeout)"""
 
-  def mkProcessCall: String = s"""process($id,client,"$topicName","$id")"""
+  def mkProcessCall: String = s"""process($id,client,[${topicNames.map(t => s"""'$t'""").mkString(",")}],"$id")"""
 }
